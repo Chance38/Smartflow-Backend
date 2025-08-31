a400094b271ed0ad9c36a0906f901849720fab5e
chance38 <chance89308@gmail.com>
2025-08-31 16:50:24 +0800

    feat: Add Middleware

---
+++ SmartFlowBackend/Middleware/Middleware.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Middleware;
+
+public class ServiceMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ServiceMiddleware> _logger;
+
+    public ServiceMiddleware(RequestDelegate next, ILogger<ServiceMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Unexpected exception.");
+
+            context.Response.StatusCode = 500;
+            context.Response.ContentType = "application/json";
+            await context.Response.WriteAsJsonAsync(new
+            {
+                RequestId = context.TraceIdentifier.ToString(),
+                ErrorMessage = "Unexpected error, Please contact support with the request ID."
+            }, new System.Text.Json.JsonSerializerOptions
+            {
+                PropertyNamingPolicy = null
+            });
+        }
+    }
+
+    public static string GetRequestId(HttpContext context)
+    {
+        return context.TraceIdentifier.ToString();
+    }
+}
